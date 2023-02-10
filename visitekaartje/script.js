21f4bae01e6006ffc1f39109948cbe0f4fbf41a2
rvdegroen <rvdegroen@gmail.com>
2023-02-10 02:50:10 +0100

    finished the front of the card and js

--- visitekaartje/script.js
+++ visitekaartje/script.js
@@ -1,7 +1,7 @@
 // give temporarily access on https://cors-anywhere.herokuapp.com/corsdemo
 const squadUrl =
   "https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/squad?id=cldcspecf0z0o0bw59l8bwqim";
-let h1 = document.querySelector("h1");
+let $name = document.querySelector(".name");
 
 // myData returns everything of the API, squad returns all the data of the squad, members returns all the member data, since I have an index number of 20, I use [20]
 const writeBusinessCardInfo = async (API) => {
@@ -9,7 +9,7 @@ const writeBusinessCardInfo = async (API) => {
     .then((response) => response.json())
     .then((myData) => myData.squad.members[20]);
   console.log(myData);
-  h1.innerHTML = myData.name;
+  $name.innerHTML = myData.name;
 };
 
 writeBusinessCardInfo(squadUrl);
