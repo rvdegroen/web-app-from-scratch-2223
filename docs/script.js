2c1f15c0c6ad0e67f462391be06166a3efb2bdb0
rvdegroen <rvdegroen@gmail.com>
2023-02-11 19:58:44 +0100

    updated docs folder with visitekaartje

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,15 @@
-// give temporarily access on https://cors-anywhere.herokuapp.com/corsdemo
-const squadUrl =
-  "https://cors-anywhere.herokuapp.com/https://whois.fdnd.nl/api/v1/squad?id=cldcspecf0z0o0bw59l8bwqim";
+// Variables
+const myMemberUrl = "https://whois.fdnd.nl/api/v1/member?id=cldepct6w3x5w0av02sp6c4gh";
+const myMemberUrlSlug = "https://whois.fdnd.nl/api/v1/member/roshnie";
+// HTML elements
 let $name = document.querySelector(".name");
 
-// myData returns everything of the API, squad returns all the data of the squad, members returns all the member data, since I have an index number of 20, I use [20]
 const writeBusinessCardInfo = async (API) => {
   const myData = await fetch(API)
     .then((response) => response.json())
-    .then((myData) => myData.squad.members[20]);
+    .then((myData) => myData.member);
   console.log(myData);
   $name.innerHTML = myData.name;
 };
 
-writeBusinessCardInfo(squadUrl);
-// const transformHTML = () => {};
+writeBusinessCardInfo(myMemberUrl);
