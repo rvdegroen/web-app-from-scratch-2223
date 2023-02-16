7f2980be6016b3e9dc8af0b6c97278146d08e205
rvdegroen <rvdegroen@gmail.com>
2023-02-17 00:04:59 +0100

    made a start on generating random quote in html

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // Variables
-const myApi = "https://www.officeapi.dev/api/quotes/random";
+const myApi = "https://www.officeapi.dev/api/quotes/random/";
+const $paper = document.querySelector(".paper-stack");
 
-// fetch all quotes
+// fetch a random quote
 getQuote = async (api) => {
+  // variables
+  let $quote = document.querySelector("blockquote");
+  let $firstName = document.querySelector(".first-name");
+  let $surname = document.querySelector(".surname");
+
   const response = await fetch(api);
   const data = await response.json();
   console.log(data);
+
+  // write data in html
+  $quote.textContent = data.data.content;
+  $firstName.textContent = data.data.character.firstname;
+  $surname.textContent = data.data.character.lastname;
 };
 
 getQuote(myApi);
+// When I click, the arrow functuon runs getQuote
+// if I didn't use a param, I could just use getQuote as 2nd param
+$paper.addEventListener("click", () => getQuote(myApi));
 
 //fetch("https://officeapi.dev/api/quotes/random")
 //  .then((response) => response.json())
