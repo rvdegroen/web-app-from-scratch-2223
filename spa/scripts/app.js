d8ea390b47fe25bcedd781252e825797941d5e07
rvdegroen <rvdegroen@gmail.com>
2023-03-09 14:24:44 +0100

    made project functional

--- spa/scripts/app.js
+++ spa/scripts/app.js
@@ -1,38 +1,177 @@
-// Variables
-// global so I can change the api easily
-const myApi = "https://www.officeapi.dev/api/";
-// global because it's an eventlistener in global scope
-const $paper = document.querySelector(".paper-stack");
+// GET Characters -> https://officeapi.dev/api/characters/
+// GET Random Quote -> https://officeapi.dev/api/quotes/random/
+const apiBaseUrl = "https://www.officeapi.dev/api";
 
-// fetch a random quote
-const getQuote = async (api) => {
-  try {
-    // $ stands for html variables -> easier to recognize
-    const $quote = document.querySelector("blockquote");
-    const $firstName = document.querySelector("span:nth-of-type(1)");
-    const $surname = document.querySelector("span:nth-of-type(2)");
-
-    // fetch random quote
-    const quote = await (await fetch(api + "quotes/random")).json();
-    console.log(quote);
-
-    // fetch random character
-    const character = await (await fetch(api + "characters/random")).json();
-    console.log(character);
-
-    // write data in html
-    $quote.textContent = quote.data.content;
-    $firstName.textContent = quote.data.character.firstname;
-    $surname.textContent = quote.data.character.lastname;
-  } catch (error) {
-    console.error(error);
-  }
+const routes = {
+  "/": {
+    element: "pageHome",
+    init: () => {},
+  },
+  "/game": {
+    element: "pageGame",
+    init: initGame,
+  },
+  "/characters": {
+    element: "pageCharacters",
+    init: initCharacters,
+  },
 };
 
-// calling functions
-getQuote(myApi);
+// https://www.freecodecamp.org/news/javascript-debounce-example/
+function debounce(func, timeout = 300) {
+  console.log("DEBOUNCE");
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
 
-// eventlisteners
-// When I click, the arrow functuon runs getQuote
-// if I didn't use a param, I could just use getQuote as 2nd param
-$paper.addEventListener("click", () => getQuote(myApi));
+// https://stackoverflow.com/a/38571132/4409162
+function shuffleArray(array) {
+  return array.sort(() => 0.5 - Math.random());
+}
+
+async function initGame() {
+  const $quote = document.getElementById("game__quote");
+  $quote.innerHTML = "";
+  const $choices = document.getElementById("game__choices");
+  $choices.innerHTML = "";
+
+  const characters = await getCharacters();
+  const quote = await getRandomQuote();
+
+  const speaker = `${quote.character.firstname} ${quote.character.lastname}`;
+  const remainingCharacters = characters
+    .map((character) => `${character.firstname} ${character.lastname}`)
+    .filter((character) => character !== speaker);
+
+  const wrongChoices = shuffleArray(remainingCharacters).slice(0, 3);
+
+  const choices = shuffleArray([speaker, ...wrongChoices]);
+
+  $quote.innerHTML = `
+    <p>"${quote.content}"</p>
+  `;
+
+  for (const choice of choices) {
+    const button = document.createElement("button");
+    button.textContent = choice;
+    button.addEventListener("click", () => {
+      if (choice === speaker) {
+        alert("Correct!");
+      } else {
+        alert("Wrong!");
+      }
+      initGame();
+    });
+    $choices.appendChild(button);
+  }
+}
+
+async function initCharacters() {
+  updateCharactersTable();
+
+  const $filterInput = document.getElementById("characters__filter");
+  $filterInput.addEventListener(
+    "input",
+    debounce((e) => updateCharactersTable(e.target.value))
+  );
+
+  const $sortAsc = document.getElementById("characters__sortAsc");
+  $sortAsc.addEventListener("click", () => updateCharactersTable($filterInput.value, "asc"));
+
+  const $sortDesc = document.getElementById("characters__sortDesc");
+  $sortDesc.addEventListener("click", () => updateCharactersTable($filterInput.value, "desc"));
+}
+
+async function updateCharactersTable(filter = null, sortDirection = null) {
+  const $table = document.getElementById("characters__tbody");
+  $table.innerHTML = "";
+
+  let characters = await getCharacters();
+
+  if (filter) {
+    characters = characters.filter(
+      (character) =>
+        character.firstname.toLowerCase().includes(filter.toLowerCase()) ||
+        character.lastname.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+
+  // https://stackoverflow.com/a/45544166/4409162
+  if (sortDirection) {
+    characters = characters.sort((a, b) =>
+      sortDirection === "asc"
+        ? a.firstname.localeCompare(b.firstname)
+        : b.firstname.localeCompare(a.firstname)
+    );
+  }
+
+  for (const character of characters) {
+    const row = document.createElement("tr");
+
+    const firstName = document.createElement("td");
+    firstName.textContent = character.firstname;
+    row.appendChild(firstName);
+
+    const lastName = document.createElement("td");
+    lastName.textContent = character.lastname;
+    row.appendChild(lastName);
+
+    $table.appendChild(row);
+  }
+}
+
+async function getRandomQuote() {
+  const response = await fetch(`${apiBaseUrl}/quotes/random`);
+  const json = await response.json();
+  return json.data;
+}
+
+async function getCharacters() {
+  const response = await fetch(`${apiBaseUrl}/characters`);
+  const json = await response.json();
+  return json.data;
+}
+
+function getPath() {
+  // localhost:3000 -> ""
+  // localhost:3000/ -> ""
+  // localhost:300/# -> ""
+  // localhost:3000/#/ -> "#/"
+  // localhost:3000/#/about -> #/about
+  const hash = window.location.hash;
+
+  if (hash === "") {
+    return "/";
+  }
+
+  return hash.slice(1);
+}
+
+function renderPage() {
+  const pages = document.querySelectorAll(".page");
+
+  // Hide all pages
+  for (const page of pages) {
+    if (!page.classList.contains("hidden")) {
+      page.classList.add("hidden");
+    }
+  }
+
+  // Show correct page
+  const path = getPath();
+  const route = routes[path];
+  const page = document.getElementById(route.element);
+  page.classList.remove("hidden");
+  route.init();
+}
+
+// Render the new page on navigation
+window.onhashchange = () => renderPage();
+
+// Render the correct page initially - required for page refresh to work
+renderPage();
