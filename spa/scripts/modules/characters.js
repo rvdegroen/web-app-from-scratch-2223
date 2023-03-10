371f3965e6b68692289577e304b8c540af60621b
rvdegroen <rvdegroen@gmail.com>
2023-03-10 04:42:10 +0100

    added extra comments for myself

--- spa/scripts/modules/characters.js
+++ spa/scripts/modules/characters.js
@@ -3,16 +3,22 @@ import { debounce } from "./helper.js";
 
 export async function initCharacters() {
   updateCharactersTable();
-
+  // input event runs when you input smthn
   const $filterInput = document.getElementById("characters__filter");
   $filterInput.addEventListener(
     "input",
+    // debounce runs the function updatechar
+    // event.target = input field
+    // value is whatever u put in there
     debounce((e) => updateCharactersTable(e.target.value))
   );
 
+  //   sort button
   const $sortAsc = document.getElementById("characters__sortAsc");
+  //   the first param in updatechar is the filter, 2nd param is how to sort it
   $sortAsc.addEventListener("click", () => updateCharactersTable($filterInput.value, "asc"));
 
+  //   sort button
   const $sortDesc = document.getElementById("characters__sortDesc");
   $sortDesc.addEventListener("click", () => updateCharactersTable($filterInput.value, "desc"));
 }
@@ -24,6 +30,7 @@ async function updateCharactersTable(filter = null, sortDirection = null) {
   let characters = await getCharacters();
 
   if (filter) {
+    // using filter, to filter the characters
     characters = characters.filter(
       (character) =>
         character.firstname.toLowerCase().includes(filter.toLowerCase()) ||
@@ -35,11 +42,16 @@ async function updateCharactersTable(filter = null, sortDirection = null) {
   if (sortDirection) {
     characters = characters.sort((a, b) =>
       sortDirection === "asc"
-        ? a.firstname.localeCompare(b.firstname)
-        : b.firstname.localeCompare(a.firstname)
+        ? //   localecompare looks at the comparison with strings, result will be -1, 0, 1
+          // array has a and b [1,2,3] a=1 b=2 => how do you want to compare this?
+          //   asc = 1
+          a.firstname.localeCompare(b.firstname)
+        : //   desc = -1
+          b.firstname.localeCompare(a.firstname)
     );
   }
 
+  //   generating html
   for (const character of characters) {
     const row = document.createElement("tr");
 
--- spa/scripts/modules/game.js
+++ spa/scripts/modules/game.js
@@ -12,17 +12,19 @@ export async function initGame() {
 
   const speaker = `${quote.character.firstname} ${quote.character.lastname}`;
   const remainingCharacters = characters
+    // takes object and turns into string
     .map((character) => `${character.firstname} ${character.lastname}`)
+    // remaining characters do not include the one who spoke the quote
     .filter((character) => character !== speaker);
-
+  // 3 wrong choices: remaining characters, shuffle it and take the first 3 remaining/random characters
   const wrongChoices = shuffleArray(remainingCharacters).slice(0, 3);
-
+  // make a new array with the speaker and 3 wrong choices and shuffle it
   const choices = shuffleArray([speaker, ...wrongChoices]);
 
   $quote.innerHTML = `
       <p>"${quote.content}"</p>
     `;
-
+  // use the choices to make the buttons
   for (const choice of choices) {
     const button = document.createElement("button");
     button.textContent = choice;
@@ -32,6 +34,7 @@ export async function initGame() {
       } else {
         alert("Wrong!");
       }
+      //   run the game again
       initGame();
     });
     $choices.appendChild(button);
